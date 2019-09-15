69c0ef354f9519bc67c28943394a7fca407286fd
xhfl160321 <42793742+xhfl160321@users.noreply.github.com>
2019-09-16 00:14:14 +0900

    Add files via upload

---
+++ routes/access.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+const path = require('path');
+
+router.get('/',function(req, res){
+    console.log('승인 창');
+    
+});
+
+module.exports = router;
