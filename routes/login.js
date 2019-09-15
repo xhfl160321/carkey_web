2f9d76377ed8af1f293cdd55bbc1223034ab18c7
xhfl160321 <42793742+xhfl160321@users.noreply.github.com>
2019-09-16 00:15:10 +0900

    Add files via upload

--- routes/login.js
+++ routes/login.js
@@ -2,45 +2,38 @@ var express = require('express');
 var router = express.Router();
 const path = require('path');
 var session = require('express-session');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-
-passport.serializeUser(function(manager, done) {
-    done(null, manager.id);
-});
-
-passport.deserializeUser(function(id, done) {
-    done(err, user);
-});
-
-passport.use(new LocalStrategy({
-    idField: 'id',
-    passwordField: 'pw',
-    session: true,
-}, function(id, password, done) {
-    if (err) {
-        return done(err);
-    }
-    if (!manager) {
-        return done(null, false, {message : '아이디가 틀렸습니다.'});
-    }
-    if (manager.password !== password) {
-        return done(null, false, {message : '비밀번호가 틀렸습니다.'});
-    }
-    if(manager === 'admin' && manager.password === 'friend'){
-        return done(null, manager);
-    }
-}));
 
 router.get('/', function(req, res){
     console.log('로그인 창');
     res.sendFile(path.join(__dirname, '../views/login.html'));
 });
 
-router.post('/', passport.authenticate('local', {
-    successRedirect : '/access',
-    failureRedirect : '/',
-    failureFlash : true
-}));
+router.post('/', function(req, res){
+    var user = {
+        userid : 'admin',
+        password : 'friend'
+    };
+
+    var id =  req.body.userid;
+    var pwd = req.body.password;
+    //아이디와 비번이 일치할 경우 승인 페이지로 넘어감
+    if(id===user.userid && pwd === user.password){
+        res.redirect('/access');
+    }
+    if(id!==user.userid){
+        console.log('아이디 오류');
+        res.send(`
+        <form action="/" method="post" onsubmit="return confirm('아이디가 일치하지 않습니다.')">
+        </form>
+        `);
+    }
+    if(pwd!==user.password){
+        console.log('비밀번호 오류');
+        res.send(`
+        <form action="/" method="post" onsubmit="return confirm('비밀번호가 일치하지 않습니다.')">
+        </form>
+        `);
+    }
+});
 
 module.exports = router;
